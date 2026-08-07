bdfd4d7e6db3efb49e79fba7e87364f49c29239b
Sam Lariviere <samlarivs@outlook.com>
2026-08-06 20:33:54 -0400

    test: observe failing test in github actions

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest';
 import { sum } from './sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(4);
 });
 
 test('adds negative numbers', () => {
